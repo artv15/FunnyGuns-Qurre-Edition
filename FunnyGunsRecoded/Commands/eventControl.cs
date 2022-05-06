206c1d58e131cef25170e8969fffcdac70a50431
artv15 <35376120+artv15@users.noreply.github.com>
2022-05-06 19:33:14 +0300

    Added perm check, but it's very bad, because Qurre has no fucking permission system!

--- FunnyGunsRecoded/Commands/eventControl.cs
+++ FunnyGunsRecoded/Commands/eventControl.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CommandSystem;
+﻿using CommandSystem;
 using MEC;
-using UnityEngine.Networking;
 
 namespace FunnyGunsRecoded.Commands
 {
@@ -20,84 +14,92 @@ namespace FunnyGunsRecoded.Commands
 
         public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
         {
-            var args = arguments.Array;
-            if (args.Length == 1)
+            if (sender.CheckPermission(PlayerPermissions.RoundEvents))
             {
-                response = "<color=green>FunnyGuns -> fg_event</color> \n" +
-                    "<color=yellow>fg_event</color> <color=green>start</color> - <color=green>Starts the event</color>\n" +
-                    "<color=yellow>fg_event</color> <color=green>stop</color> - <color=green>Stops the event</color>";
-                return true;
-            }
-            if (args.Length == 2)
-            {
-                switch (args[1].ToLower())
+                var args = arguments.Array;
+                if (args.Length == 1)
                 {
-                    case "start":
-                        if (!Plugin.isEngaged)
-                        {
-                            Plugin.isEngaged = true;
-                            Timing.RunCoroutine(Coroutines.GameEvent.gameController(), "gameController");
-                            Timing.RunCoroutine(Coroutines.PlayerHUD.hudCoroutine(), "playerHUD");
-
-                            response = "Success! Event is now started!";
-                            return true;
-                        }
-                        else
-                        {
-                            response = "Event is running already!";
-                            return true;
-                        }
-                        break;
-                    case "stop":
-                        if (Plugin.isEngaged)
-                        {
-                            var ev = new Events();
-                            ev.StopAllEventShit();
-                            response = "Success! Event is now stopped!";
-                            return true;
-                        }
-                        else
-                        {
-                            response = "Event is stopped already!";
-                            return true;
-                        }
-                        break;
-                    default:
-                        if (Plugin.IsDebugEnabled)
-                        {
-                            response = "Arguments given: \n";
-                            for (int i = 0; i < args.Length; i++)
+                    response = "<color=green>FunnyGuns -> fg_event</color> \n" +
+                        "<color=yellow>fg_event</color> <color=green>start</color> - <color=green>Starts the event</color>\n" +
+                        "<color=yellow>fg_event</color> <color=green>stop</color> - <color=green>Stops the event</color>";
+                    return true;
+                }
+                if (args.Length == 2)
+                {
+                    switch (args[1].ToLower())
+                    {
+                        case "start":
+                            if (!Plugin.isEngaged)
                             {
-                                response += "ID: " + i + ". Argument: " + args[i] + "\n";
-                            }
+                                Plugin.isEngaged = true;
+                                Timing.RunCoroutine(Coroutines.GameEvent.gameController(), "gameController");
+                                Timing.RunCoroutine(Coroutines.PlayerHUD.hudCoroutine(), "playerHUD");
 
-                            return true;
-                        }
-                        else
+                                response = "Success! Event is now started!";
+                                return true;
+                            }
+                            else
+                            {
+                                response = "Event is running already!";
+                                return true;
+                            }
+                            break;
+                        case "stop":
+                            if (Plugin.isEngaged)
+                            {
+                                var ev = new Events();
+                                ev.StopAllEventShit();
+                                response = "Success! Event is now stopped!";
+                                return true;
+                            }
+                            else
+                            {
+                                response = "Event is stopped already!";
+                                return true;
+                            }
+                            break;
+                        default:
+                            if (Plugin.IsDebugEnabled)
+                            {
+                                response = "Arguments given: \n";
+                                for (int i = 0; i < args.Length; i++)
+                                {
+                                    response += "ID: " + i + ". Argument: " + args[i] + "\n";
+                                }
+
+                                return true;
+                            }
+                            else
+                            {
+                                response = "Unknown argument(s). Check all subcommands using <color=yellow>fg_event</color>";
+                                return false;
+                            }
+                            break;
+                    }
+                }
+                else
+                {
+                    if (Plugin.IsDebugEnabled)
+                    {
+                        response = "Arguments given: \n";
+                        for (int i = 0; i < args.Length; i++)
                         {
-                            response = "Unknown argument(s). Check all subcommands using <color=yellow>fg_event</color>";
-                            return false;
+                            response += "ID: " + i + ". Argument: " + args[i] + "\n";
                         }
-                        break;
+
+                        return true;
+                    }
+                    else
+                    {
+                        response = "Unknown argument(s). Check all subcommands using <color=yellow>fg_event</color>";
+                        return false;
+                    }
                 }
             }
             else
             {
-                if (Plugin.IsDebugEnabled)
-                {
-                    response = "Arguments given: \n";
-                    for (int i = 0; i < args.Length; i++)
-                    {
-                        response += "ID: " + i + ". Argument: " + args[i] + "\n";
-                    }
-
-                    return true;
-                }
-                else
-                {
-                    response = "Unknown argument(s). Check all subcommands using <color=yellow>fg_event</color>";
-                    return false;
-                }
+                response = "Not enough permissions! Required: RoundEvents.";
+                return false;
             }
         }
     }
